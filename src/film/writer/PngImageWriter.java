c4928f02ee87d9e856f65c40efb64c5a68ce4bd2
ben <bwu217@gmail.com>
2018-11-18 16:59:22 -0800

    Add logic to create image output directory if needed

--- src/film/writer/PngImageWriter.java
+++ src/film/writer/PngImageWriter.java
@@ -7,9 +7,11 @@ import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class PngImageWriter implements ImageWriter
 {
+    private static final Logger logger = Logger.getLogger(PngImageWriter.class.getName());
     private final String fileName;
 
     public PngImageWriter(String fileName)
@@ -21,6 +23,13 @@ public class PngImageWriter implements ImageWriter
     public void writeImage(int resolutionX, int resolutionY, double[] pixels) throws IOException
     {
         File imageFile = new File(fileName);
+        if (!imageFile.exists())
+        {
+            if (!imageFile.getParentFile().mkdirs())
+            {
+                logger.severe("Failed to create output directory " + imageFile.getParentFile().getAbsolutePath());
+            }
+        }
 
         BufferedImage img = new BufferedImage(resolutionX, resolutionY, BufferedImage.TYPE_INT_RGB);
         WritableRaster raster = (WritableRaster) img.getData();
